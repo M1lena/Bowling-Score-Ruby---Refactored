c69610b297c9638d61004ff3c41c1b230f09986d
M1lena <stoevami@gmail.com>
2015-04-20 16:05:08 +0100

    first frame score tests pass

--- lib/bowling_score.rb
+++ lib/bowling_score.rb
@@ -1,18 +1,28 @@
 require_relative 'bowling_frame'
-
+# new
 class BowlingScores
+  attr_accessor :first_frame, :second_frame, :third_frame
   attr_reader :score
 
   def initialize
     @score = 0
   end
 
-  def first_frame_score
-    first_frame = BowlingFrame.new(roll1, roll2)
-    raw_total
+  def first_frame_score(roll1, roll2)
+    @first_frame = BowlingFrame.new(roll1, roll2)
+    first_frame.raw_total
+    # first_frame.spare?
   end
 
   def second_frame_score
-    second_frame = BowlingFrame.new(roll1, roll2)
+    @second_frame = BowlingFrame.new(roll1, roll2)
   end
 end
+
+
+game = BowlingScores.new
+
+puts game.first_frame_score(2,3)
+puts game.first_frame.spare?
+
+
